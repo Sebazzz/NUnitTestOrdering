2e275eaf289d0db8db86ae0a486d6df4853a089b
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2016-09-16 12:12:49 +0200

    Add extra Integrity test

---
+++ src/NUnitTestOrdering.Tests/TestData/TestIntegrity/ActionAttributeRunsOnEverything/Test.cs
@@ -0,0 +1,54 @@
+﻿// ******************************************************************************
+//  © 2016 Sebastiaan Dammann - damsteen.nl
+// 
+//  File:           : Test.cs
+//  Project         : NUnitTestOrdering.Tests
+// ******************************************************************************
+
+using NUnitTestOrdering.Tests.TestData.TestIntegrity.ActionAttributeRunsOnEverything;
+
+[assembly: MyAction]
+namespace NUnitTestOrdering.Tests.TestData.TestIntegrity.ActionAttributeRunsOnEverything {
+    using System;
+
+    using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+
+    public sealed class MyActionAttribute : Attribute, ITestAction {
+        private readonly TestLogger _logger;
+
+        /// <inheritdoc />
+        public MyActionAttribute() {
+            this._logger = new TestLogger(this);
+        }
+
+        /// <inheritdoc />
+        public void BeforeTest(ITest test) {
+            this._logger.Log(test.MethodName);
+        }
+
+        /// <inheritdoc />
+        public void AfterTest(ITest test) {
+            this._logger.Log(test.MethodName);
+        }
+
+        /// <inheritdoc />
+        public ActionTargets Targets => ActionTargets.Suite | ActionTargets.Test;
+    }
+
+    [TestFixture]
+    public class UnorderedTest1 : LoggingTestBase {
+        [Test]
+        public void Execute() {
+            this.Log();
+        }
+    }
+
+    [TestFixture]
+    public class UnorderedTest2 : LoggingTestBase {
+        [Test]
+        public void Execute() {
+            this.Log();
+        }
+    }
+}
